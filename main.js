351208454528573e1d348d71b8905612e293a99b
Ishrat <47534248+ishratUmar18@users.noreply.github.com>
2023-01-04 22:02:22 +0500

    Update main.js

--- main.js
+++ main.js
@@ -57,7 +57,7 @@ function addStar() {
 }
 Array(200).fill().forEach(addStar)
 
-const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+const spaceTexture = new THREE.TextureLoader().load('/main/space.jpg');
 scene.background = spaceTexture;
 
 // Texture mapping - Avator
@@ -70,8 +70,8 @@ const ishrat = new THREE.Mesh(
 scene.add(ishrat);
 
 // Moon
-const moonTexture = new THREE.TextureLoader().load('moon.jpg');
-const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+const moonTexture = new THREE.TextureLoader().load('../moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('/blob/main/normal.jpg');
 
 const moon = new THREE.Mesh(
     new THREE.SphereGeometry(3, 32, 32),
